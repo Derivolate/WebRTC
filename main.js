2f44f7f6e8fe7309340e23a51eb7e1456e46b226
XD-face-me <Bastiaan97.bvh@gmail.com>
2013-12-13 22:34:05 +0100

    bit'o cleanup some local libs Finding errors....

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //constants
 
-var AUDIO = false;
+var AUDIO = true;
 var VIDEO = true;
 
 var width = 200;
 var height = 150;
 var vids = 4;
-//var video = [$('#vid0'),$('#vid1')];
 // Compatibility shim
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 // PeerJS object (default key)
 //TODO set up own peerjs server https://github.com/peers/peerjs-server
 //TODO set up/get STUN and TURN server.
-var peer = new Peer({key: 'wzjo8kfky0qz1tt9'});
+var peer = new Peer({key: 'wzjo8kfky0qz1tt9'
+});
 peer.on('open', function() {
     $('#my-id').text(peer.id);
 });
@@ -24,10 +24,10 @@ peer.on('call', function(call) {
     call.answer(window.localStream);
     step3(call);
 });
+
 peer.on('error', function(err) {
     alert(err.message);
-    // Return to step 2 if error occurs
-    step2();
+    stopCall();
 });
 
     start();
@@ -43,9 +43,6 @@ $(function() {
     $('#end-call').click(function() {
         window.existingCall.close();
     });
-
-    // Get things started
-
 });
 
 function start() {
@@ -80,5 +77,74 @@ function step3(call) {
 
     // UI stuff
     window.existingCall = call;
+    $('#their-id').text(call.peer);
     call.on('close', stopCall());
 }
+
+/*
+ 
+ 
+    peer.on('open', function(){
+      $('#my-id').text(peer.id);
+    });
+
+    // Receiving a call
+    peer.on('call', function(call){
+      call.answer(window.localStream);
+      step3(call);
+    });
+    peer.on('error', function(err){
+      alert(err.message);
+      step2();
+    });
+
+    $(function(){
+      $('#make-call').click(function(){
+      
+        var call = peer.call($('#callto-id').val(), window.localStream);
+
+        step3(call);
+      });
+
+      $('#end-call').click(function(){
+        window.existingCall.close();
+        step2();
+      });
+
+      $('#step1-retry').click(function(){
+        $('#step1-error').hide();
+        step1();
+      });
+
+      step1();
+    });
+
+    function step1 () {
+      navigator.getUserMedia({audio: true, video: true}, function(stream){
+        $('#my-video').prop('src', URL.createObjectURL(stream));
+
+        window.localStream = stream;
+        step2();
+      }, function(){ $('#step1-error').show(); });
+    }
+
+    function step2 () {
+      $('#step1, #step3').hide();
+      $('#step2').show();
+    }
+
+    function step3 (call) {
+      if (window.existingCall) {
+        window.existingCall.close();
+      }
+
+      call.on('stream', function(stream){
+        $('#their-video').prop('src', URL.createObjectURL(stream));
+      });
+
+      window.existingCall = call;
+      call.on('close', step2);
+      $('#step1, #step2').hide();
+      $('#step3').show();
+    }
+* */
